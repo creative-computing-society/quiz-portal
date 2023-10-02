0fb164e7e9257c14ffe03dec197c9e5db1ce1ba5
Akarsh <akarsh91140@gmail.com>
2023-10-02 16:25:59 +0530

    updated config.js for deployment

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
-const api_url = "http://127.0.0.1:8000/api/v1"
-const slot1_time = "November, 11, 2022 21:00:00"
-const slot2_time = "November, 11, 2022 1:31:35"
+const api_url = "http://api.recruitments.ccstiet.com"
+const slot1_time = "October, 09, 2023 21:00:00"
+const slot2_time = "October, 09, 2023 22:00:00"
 const test_duration = 15
 
 // const url = "http://127.0.0.1:8000/api/v1/users/login";
