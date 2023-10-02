de92722385dd0ef883ffd477ff24749f65b74e11
Akarsh <akarsh91140@gmail.com>
2023-10-02 17:30:08 +0530

    Emails Updated

--- frontend/src/Pages/Home/Components/Carousel.js
+++ frontend/src/Pages/Home/Components/Carousel.js
@@ -15,9 +15,9 @@ import styles from "../Style/carousel.module.css";
 
 
 
-import img1 from "../Assets/img1.JPG";
+import img1 from "../Assets/img1.jpg";
 import img2 from "../Assets/img2.jpg";
-import img3 from "../Assets/img3.JPG";
+import img3 from "../Assets/img3.jpg";
 import img4 from "../Assets/img4.jpg";
 import img5 from "../Assets/img5.jpg";
 
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
-const api_url = "http://api.recruitments.ccstiet.com"
-const slot1_time = "October, 09, 2023 21:00:00"
-const slot2_time = "October, 09, 2023 22:00:00"
+const api_url = "https://api.recruitments.ccstiet.com"
+const slot1_time = "October, 10, 2023 22:00:00"
+const slot2_time = "October, 10, 2023 23:00:00"
 const test_duration = 15
 
 // const url = "http://127.0.0.1:8000/api/v1/users/login";
