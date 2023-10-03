3776dbd3501237305a588eda2e6e6798d2b90cdd
Akarsh <akarsh91140@gmail.com>
2023-10-03 23:34:01 +0530

    Image Saving Deployment FIX

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,8 +1,12 @@
-exports.imageDecode= (image,imageName) => {
+exports.imageDecode = (image, imageName) => {
     const fs = require('fs');
     const base64String = image;
     const base64Data = base64String.replace(/^data:image\/\w+;base64,/, '');
     const imageBuffer = Buffer.from(base64Data, 'base64');
-    // Save the buffer as an image file
-    fs.writeFileSync('./images/'+imageName+'.jpg', imageBuffer);
+    try {
+        fs.writeFileSync('/container/path/images/' + imageName + '.jpg', imageBuffer);
+        console.log('Image saved successfully.');
+    } catch (error) {
+        console.error('Error saving the image:', error);
+    }
 }
